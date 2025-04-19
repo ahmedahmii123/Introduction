2250905acef53a396bc51fa54e19872a01277e02
muhammad ahmed <139594182+ahmedahmii123@users.noreply.github.com>
2025-04-19 11:15:20 -0700

    Add files via upload

---
+++ introduction.dart
@@ -0,0 +1,11 @@
+void main() {
+  String name = "Jawwad";
+  int age = 23;
+  String stdclass = "Matric";
+  double per = 56.5;
+
+  print("hello my name is $name");
+  print("my age is $age");
+  print("my qualification is $stdclass");
+  print("my percentage was $per%");
+}
